f9bfdaa00b9fba54ceda03c57a6a8ce70a63e626
moe-elsadig <m.da7th@gmail.com>
2019-05-21 13:39:08 +0300

    added the routing functions

--- vagrant/finalProject.py
+++ vagrant/finalProject.py
@@ -1,8 +1,73 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
 
+engine = create_engine('sqlite:///restaurantmenu.db', connect_args={'check_same_thread': False})
 
+Base.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+"""
+Routings:
+
+ShowRestaurants()   :   /
+                    :   /Restaurants
+
+newRestaurant()     :   /restaurant/new
+
+editRestaurant()    :   /restaurant/restaurant_id/edit
+
+deleteRestaurant()  :   /restaurant/restaurant_id/delete
+
+showMenu()          :   /restaurant/restaurant_id
+                    :   /restaurant/restaurant_id/menu
+
+newMenuItem()       :   /restaurant/restaurant_id/menu/new
+
+editMenuItem()      :   /restaurant/restaurant_id/menu/menu_id/edit
+
+deleteMenuItem()      :   /restaurant/restaurant_id/menu/menu_id/delete
+"""
+
+@app.route('/')
+@app.route('/restaurants/')
+def showRestaurants():
+    return "all restaurants"
+
+@app.route('/restaurant/new/')
+def newRestaurant():
+    return "new restaurant"
+
+@app.route('/restaurant/restaurant_id/edit/')
+def editRestaurant():
+    return "edit restaurant"
+
+@app.route('/restaurant/restaurant_id/delete/')
+def deleteRestaurant():
+    return "delete restaurant"
+
+@app.route('/restaurant/restaurant_id/')
+@app.route('/restaurant/restaurant_id/menu/')
+def showMenu():
+    return "menu"
+
+@app.route('/restaurant/restaurant_id/menu/new/')
+def newMenuItem():
+    return "new Menu Item"
+
+@app.route('/restaurant/restaurant_id/menu/menu_id/edit')
+def editMenuItem():
+    return "edit Menu Item"
+
+@app.route('/restaurant/restaurant_id/menu/menu_id/delete')
+def deleteMenuItem():
+    return "delete Menu Item"
 
 
 if __name__ == '__main__':
